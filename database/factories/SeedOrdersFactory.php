eaa8a71efd37ecbde2462cc8916517160e74b9e9
juan_bernardo_zuluaga <email@email.com>
2021-05-24 21:49:52 -0500

    auto-gen factories

---
+++ database/factories/SeedOrdersFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SeedOrders;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SeedOrdersFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = SeedOrders::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
